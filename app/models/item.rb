e63ec173f7d0d357f582fb0f58e01138f5e42e43
i-shintaro <shintaroiwashita7857@gmail.com>
2025-02-17 17:33:51 +0900

    itemモデルリファクタリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  with_options presence: true, numericality: { other_than: 0, message: "can't be blank" } do
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
     validates :shipping_charge_id
@@ -28,8 +28,7 @@ class Item < ApplicationRecord
     validates :shipping_day_id
   end
 
-  validates :price, presence: true,
-                    numericality: { only_integer: true,
+  validates :price, numericality: { only_integer: true,
                                     greater_than_or_equal_to: 300,
                                     less_than_or_equal_to: 9_999_999 }
 end
